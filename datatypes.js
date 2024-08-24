cbc8fda664c7e4ad655d5177d43fc3371452091d
umerrahim <167948749+uummeerr@users.noreply.github.com>
2024-08-24 11:10:44 +0000

    day2

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all js coede as never version
+// good to write , for good Syntax
+console.log(1
+    +23)  // code readablity should be high
+console.log("hello")
+
+
+// documentation
+// mdn
+// tc39.cs
+// different browser different standard
+// so not go go with the clash an organization make into place 
+// which define at the standards what should be the standard for the browser
+
+// datatypes
+// primitive data types
+// string 
+// Number 
+// bigint 
+// null (intensionally keeping value as nothing)
+// undefined (not assinging and valuer)
+// symbol for uniqueness
+
+
+// typeof null is object
+
