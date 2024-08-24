15129a1f814d645e03d771379632b84b53a57ba6
umerrahim <167948749+uummeerr@users.noreply.github.com>
2024-08-24 07:49:16 +0000

    day1

---
+++ varaibles.js
@@ -0,0 +1,26 @@
+// const keyword used to define varaibles whose value cannot be changed
+// const , let , already defined in compiler what to do when it is written
+// [] square brackets
+// when const keyword is defined   to a varaible its value cannot be changed 
+// const hello =234
+// hello =23  cannot do this thing for a const keyword
+// to get in tabular we can use console.table([])
+// {}  called a scope 
+// let and var (problem with scope)  (varaibles)
+// we can reserve a memory without using a keyword but it will have a scope problem
+// accountname = "hello"
+// accountname = "world" 
+// declaring variable without giving value to it , it will give the value of undefined (acccountstate)
+
+const accountId = 234
+let name = "github"
+let accountpassword = 234234
+accountemail = "hello@gmail.com"
+let accountstate;
+
+
+
+console.log(accountId)
+console.log(name)
+console.log(accountpassword)
+console.table([accountId, accountpassword, accountemail, accountstate])
