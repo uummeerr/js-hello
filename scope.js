361bce502ba00fea97c5443311375796d754a34c
umerrahim <167948749+uummeerr@users.noreply.github.com>
2024-10-13 13:34:40 +0000

    scope

--- scope.js
+++ scope.js
@@ -6,6 +6,15 @@
 // Variables declared outside a block (in a global or function scope) can be accessed inside the block.
 // Variables declared inside a block are not accessible outside the block if they are declared with let or const.
 
+
+// function scope > varaible is is accessible within the function   > fucntions scoped varaible , varaible defined with the function 
+// global scope  >   varaible is accessible wherever you call it 
+// block scope >  varaible is accesible within the block of the code  
+
+// let and const > can use 
+// var and without equals to varaible > are not scoped
+
+
 // block scope >> code in the block
 // // global scope >> code without in the block / open code
 
@@ -29,26 +38,63 @@
 //   functionVar is declared in the function scope, so it can be accessed anywhere inside the myFunction.
 // blockVar is declared inside the block (inside the if statement), so it can only be accessed within the block. When you try to access blockVar outside the if block, you'll get an error because it's block-scoped.
 
+// let a = 13
+// if(true){
+//   const  a =123
+//     b =23
+//  console.log(a)
+// }
 
+// console.log(a)
+ 
 
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// if (true){
+//   let a = 12
+//   const b = 23
+//   var c =2322
+// }
+
+// // console.log(a)
+// // console.log(b)
+// console.log(c)
+// // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //   nested scope , fucntion inside function
 
 
-function hello(){
-    const username = "umer"
+function one(){
+  const username = "umer"
+  return username
+
+  function two (){
   
+    const website = "youtube"  
+    console.log(website)
+  }
 
-    function hello2(){
-        console.log("world")
-        const h = "rahim"
-    }
-
-    // console.log(h)
-
-    hello2()
+  
 }
-// hello()
+console.log(one())
+
+
+// function ceko(){
+//   const neaaw = "hello"
+  
+//   function neww(){
+//     console.log(neaaw)
+//     const neww = "five"
+//   }
+//   // console.log(neww)
+// neww()
+// }
+// ceko()
+// Curly Braces and Scope:
+// Object Curly Braces ({}):
+
+// The curly braces in an object literal, like { username: "umer", age: 21 }, are not related to scope.
+// They are simply a way to group key-value pairs and define the structure of an object.
+// Objects do not create a scope for variables. The curly braces here are just part of the object syntax, not a scoping mechanism.
+
+
 
 
